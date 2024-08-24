646adb13b31fd14dcda701f585a56fe827c955e5
intothenetwork <109516889+intothenetwork@users.noreply.github.com>
2024-08-24 23:38:20 +0800

    Add files via upload

---
+++ frame python.py
@@ -0,0 +1,12 @@
+from flask import Flask, url_for
+from framelib import frame
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return frame(
+        image='https://opengraph.githubassets.com/0x/devinaconley/python-frames',
+        button1='next',
+        post_url=url_for('second_page', _external=True),
+    )
