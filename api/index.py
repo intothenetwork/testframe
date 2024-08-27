50158ba76db6339fd15b0c7bfeab31889bf68b01
intothenetwork <109516889+intothenetwork@users.noreply.github.com>
2024-08-27 15:19:20 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, url_for
+from framelib import frame
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return frame(
+        image='https://opengraph.githubassets.com/0x/devinaconley/python-frames',
+        button1='next',
+        post_url=url_for('second_page', _external=True),
+    )
+    
 @app.route('/hello')
 def home():
     return 'Hello, World!'
